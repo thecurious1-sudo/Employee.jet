23af5226998449fb2f2ddbddc4f309e655025a32
Vaibhav Jaiswal <jaiswalvaibhav102@gmail.com>
2022-12-17 20:43:31 +0530

    Server starts now after connection with mongoDB

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,19 +2,24 @@ require("dotenv").config({ path: require("find-config")(".env") });
 const mongoose = require("mongoose");
 // const dbUrl = "mongodb://0.0.0.0:27017/EmployeeJet";
 const dbUrl = process.env.DB_URL;
-mongoose
-  .connect(dbUrl)
-  // .connect(dbUrl)
-  .then(() => {
-    console.log("Connection established!");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-  console.log("Database connected");
-});
 
-module.exports = db;
+const connectDB = (URL) => {
+  return (
+    mongoose
+      .connect(dbUrl)
+      // .connect(dbUrl)
+      .then(() => {
+        console.log("Connection established!");
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  );
+  const db = mongoose.connection;
+  db.on("error", console.error.bind(console, "connection error:"));
+  db.once("open", () => {
+    console.log("Database connected");
+  });
+};
+
+module.exports = connectDB;
--- index.js
+++ index.js
@@ -1,40 +1,54 @@
+require("dotenv").config({ path: require("find-config")(".env") });
 const express = require("express");
 const port = 3000;
 const app = express();
-const db=require("./config/mongoose");
+const connectDB = require("./config/mongoose");
+const start = async () => {
+  try {
+    await connectDB(process.env.DB_URL);
+    app.listen(port, (err) => {
+      if (err) {
+        console.log("Error starting the server");
+      }
+      console.log("Server started on port 3000");
+    });
+  } catch (error) {
+    console.log(error);
+    console.log("Failed to connect to the database, server is not running.");
+  }
+};
 const path = require("path");
 const methodOverride = require("method-override");
 
 app.use(methodOverride("_method"));
 app.use(express.urlencoded());
 app.use(express.json());
-app.set(`view engine` , `ejs`);
+app.set(`view engine`, `ejs`);
 app.set(`views`, path.join(__dirname, `views`));
 app.use(express.static(`./asset`));
 
+app.use(`/uploads`, express.static(__dirname + `/uploads`));
 
-app.use(`/uploads` , express.static(__dirname + `/uploads`));
-
-const expressLayouts =  require(`express-ejs-layouts`);
+const expressLayouts = require(`express-ejs-layouts`);
 app.use(expressLayouts);
 // Extracting styles and scripts
-app.set(`layout extractStyles` , true);
-app.set(`layout extractScripts` , true);
+app.set(`layout extractStyles`, true);
+app.set(`layout extractScripts`, true);
 
 // For User Authentication
-const session = require('express-session');
-const passport = require('passport');
-const passportLocal = require('./config/passportLocalStrategies');
+const session = require("express-session");
+const passport = require("passport");
+const passportLocal = require("./config/passportLocalStrategies");
 
-
-app.use(session({
+app.use(
+  session({
     name: `Employee.Jet`,
     // TODO change the secret before deployment in production mode
     secret: `blahblah`,
     saveUninitialized: false, //It means that data will not be saved in cookies until signin is there
     resave: false,
     cookie: {
-        maxAge: (1000*6000*1000)
+      maxAge: 1000 * 6000 * 1000,
     },
     // store: new MongoStore({
     //     mongoUrl: 'mongodb://localhost/EmployeeJet',
@@ -44,7 +58,8 @@ app.use(session({
     //     console.log(err || `connect mongo ok`);
     // }
     // )
-}));
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -57,16 +72,7 @@ app.use(flash());
 const customM = require(`./config/middleware`);
 app.use(customM.setFlash);
 
-app.use('/', require('./routes'));
-
+app.use("/", require("./routes"));
 
 //start server
-app.listen(port, err => {
-
-    if(err)
-    {
-        console.log("Error starting the server");
-    }
-    console.log("Server started on port 3000");
-});
-
+start();
